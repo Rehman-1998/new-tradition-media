624bf803ac455f90a2e4b4153f36a6eec4f08019
sameer <sameerjahangier99@gmail.com>
2023-04-02 02:58:35 +0500

    canvas added

--- js/index.js
+++ js/index.js
@@ -1,49 +1,46 @@
 //  AOS Library
 AOS.init();
 
-
-
 // Add background class of stick header
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('header');
-    const scrollPosition = window.scrollY;
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  const scrollPosition = window.scrollY;
 
-    if (scrollPosition > 0) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
-
-  // play-pause video
-  function playToggleVideo() {
-    var video = document.getElementById("my-video");
-    var background = document.querySelector(".controller-background");
-    
-    if (video.paused) {
-      video.play();
-      background.classList.remove("play");
-      background.classList.add("pause");
-    } else {
-      video.pause();
-      background.classList.remove("pause");
-      background.classList.add("play");
-    }
+  if (scrollPosition > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
   }
+});
 
+// play-pause video
+function playToggleVideo() {
+  var video = document.getElementById("my-video");
+  var background = document.querySelector(".controller-background");
 
-    // mute-umute video
-    function soundToggleVideo() {
-      var video = document.getElementById("my-video");
-      var background = document.querySelector(".mic-background");
-      
-      if (video.muted) {
-        video.muted = false;
-        background.classList.remove("mute");
-        background.classList.add("volume");
-      } else {
-        video.muted = true;
-        background.classList.remove("volume");
-        background.classList.add("mute");
-      }
-    }
+  if (video.paused) {
+    video.play();
+    background.classList.remove("play");
+    background.classList.add("pause");
+  } else {
+    video.pause();
+    background.classList.remove("pause");
+    background.classList.add("play");
+  }
+}
+
+// mute-umute video
+function soundToggleVideo() {
+  var video = document.getElementById("my-video");
+  var background = document.querySelector(".mic-background");
+
+  if (video.muted) {
+    video.muted = false;
+    background.classList.remove("mute");
+    background.classList.add("volume");
+  } else {
+    video.muted = true;
+    background.classList.remove("volume");
+    background.classList.add("mute");
+  }
+}
