7b9dce17b6395ccc3de1f3bf300a548c9e19ca7b
sameer <sameerjahangier99@gmail.com>
2023-04-01 22:51:09 +0500

    hero section video working

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 //  AOS Library
 AOS.init();
+
+
+
+// Add background class of stick header
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
