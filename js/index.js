e5a5a0ce5a91933cd41c585f87af9dc65b81c972
sameer <sameerjahangier99@gmail.com>
2023-04-02 01:19:27 +0500

    hamburger menu and video controllers working

--- js/index.js
+++ js/index.js
@@ -14,3 +14,36 @@ window.addEventListener('scroll', function () {
       header.classList.remove('scrolled');
     }
   });
+
+  // play-pause video
+  function playToggleVideo() {
+    var video = document.getElementById("my-video");
+    var background = document.querySelector(".controller-background");
+    
+    if (video.paused) {
+      video.play();
+      background.classList.remove("play");
+      background.classList.add("pause");
+    } else {
+      video.pause();
+      background.classList.remove("pause");
+      background.classList.add("play");
+    }
+  }
+
+
+    // mute-umute video
+    function soundToggleVideo() {
+      var video = document.getElementById("my-video");
+      var background = document.querySelector(".mic-background");
+      
+      if (video.muted) {
+        video.muted = false;
+        background.classList.remove("mute");
+        background.classList.add("volume");
+      } else {
+        video.muted = true;
+        background.classList.remove("volume");
+        background.classList.add("mute");
+      }
+    }
