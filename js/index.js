32a65d4564e681b56816895b23f3a017483abc04
sameer <sameerjahangier99@gmail.com>
2023-05-10 02:15:25 +0500

    video added on the content creation section

--- js/index.js
+++ js/index.js
@@ -34,3 +34,37 @@ function soundToggleVideo() {
 }
 
 
+
+// play-pause video
+function playContentCreationVideoToggleVideo() {
+  var video = document.getElementById("my-video-2");
+  var background = document.querySelector(".content-controller-background");
+
+  if (video.paused) {
+    video.play();
+    background.classList.remove("fa-circle-play");
+    background.classList.add("fa-circle-pause");
+  } else {
+    video.pause();
+    background.classList.add("fa-circle-play");
+    background.classList.remove("fa-circle-pause");
+  }
+}
+
+// mute-umute video
+function soundContentCreationVideoToggleVideo() {
+  var video = document.getElementById("my-video-2");
+  var background = document.querySelector(".content-mic-background");
+
+  if (video.muted) {
+    video.muted = false;
+    background.classList.remove("fa-volume-xmark");
+    background.classList.add("fa-volume-high");
+  } else {
+    video.muted = true;
+    background.classList.remove("fa-volume-high");
+    background.classList.add("fa-volume-xmark");
+  }
+}
+
+
