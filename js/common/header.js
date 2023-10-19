ad13760abb088dec17d9435a6a710a3923b981df
Abdul Rehman <abdulrehmanazmat007@gmail.com>
2023-10-20 00:10:09 +0500

    International Page added

--- js/common/header.js
+++ js/common/header.js
@@ -2,15 +2,20 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   const hamburgerIcon = document.getElementById("header-icon");
+  const callUs = document.getElementById("call-us-button");
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 0) {
     header.classList.add("scrolled");
     hamburgerIcon.classList.remove("light-icon");
     hamburgerIcon.classList.add("dark-icon");
+    callUs.classList.remove("light-call-us-button");
+    callUs.classList.add("dark-call-us-button");
   } else {
     header.classList.remove("scrolled");
     hamburgerIcon.classList.add("light-icon");
     hamburgerIcon.classList.remove("dark-icon");
+    callUs.classList.add("light-call-us-button");
+    callUs.classList.remove("dark-call-us-button");
   }
 });
